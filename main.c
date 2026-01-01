674abb16f5daf1aa3b4765ceda55bddeb835b627
samanbeer <tomas.pachomov@frengp.cz>
2026-01-01 19:47:21 +0100

    Improved design.

--- main.c
+++ main.c
@@ -4,21 +4,22 @@
 #include <conio.h>
 #include <windows.h>
 #include <direct.h>
+#include <ctype.h>
 
 #define MAX_SERVERS 20
 #define MAX_STR 128
 #define MAX_PATH_LEN 512
 #define ENCRYPT_KEY 0xAF
 
-// --- ROZŠÍŘENÁ BAREVNÁ PALETA ---
-#define COL_FRAME 9       // Tmavě modrá pro rámečky
-#define COL_CYAN 11       // Hlavní barva (text)
-#define COL_GREEN 10      // Úspěch / Přidat
-#define COL_GRAY 8        // Méně důležité info
-#define COL_RED 12        // Nebezpečí / Exit
-#define COL_YELLOW 14     // Výstraha / Klíče
-#define COL_WHITE 15      // Jasný text
-#define COL_BG_BAR 240    // Inverzní barva pro status bar
+// --- COLOR PALETTE ---
+#define COL_FRAME 9       
+#define COL_CYAN 11       
+#define COL_GREEN 10      
+#define COL_GRAY 8        
+#define COL_RED 12        
+#define COL_YELLOW 14     
+#define COL_WHITE 15      
+#define COL_BG_BAR 240    
 #define COL_DEFAULT 7
 
 typedef struct {
@@ -49,9 +50,62 @@ void hideCursor() {
     SetConsoleCursorInfo(consoleHandle, &info);
 }
 
+void showCursor() {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = TRUE;
+    SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void fixEncoding() {
+    SetConsoleOutputCP(852);
+    SetConsoleCP(852);
+}
+
 void cipher(char *data) {
     if (!data) return;
     for(int i = 0; i < (int)strlen(data); i++) data[i] ^= ENCRYPT_KEY;
+}
+
+void inputWithPlaceholder(char* buffer, int maxLen, const char* placeholder, int x, int y) {
+    int pos = 0;
+    int key;
+    
+    gotoxy(x, y);
+    setCol(COL_GRAY);
+    printf("%s", placeholder);
+    gotoxy(x, y); 
+    
+    showCursor();
+    while (1) {
+        key = _getch();
+        if (key == 13) { // ENTER
+            buffer[pos] = '\0';
+            break;
+        } else if (key == 8) { // BACKSPACE
+            if (pos > 0) {
+                pos--;
+                printf("\b \b");
+                if (pos == 0) {
+                    gotoxy(x, y);
+                    setCol(COL_GRAY);
+                    printf("%s", placeholder);
+                    gotoxy(x, y);
+                }
+            }
+        } else if (key >= 32 && key <= 126 && pos < maxLen - 1) {
+            if (pos == 0) {
+                gotoxy(x, y);
+                for (int i = 0; i < (int)strlen(placeholder); i++) printf(" ");
+                gotoxy(x, y);
+                setCol(COL_CYAN);
+            }
+            buffer[pos++] = (char)key;
+            printf("%c", key);
+        }
+    }
+    hideCursor();
 }
 
 // --- LOGIKA SOUBORŮ ---
@@ -90,8 +144,8 @@ void drawFrame(int x, int y, int w, int h, int color, char* title) {
     printf("%c", 187);
 
     for(int i=1; i<h; i++) {
-        gotoxy(x, y+i); printf("%c ", 186);
-        gotoxy(x+w+1, y+i); printf(" %c", 186);
+        gotoxy(x, y+i); printf("%c", 186); // OPRAVENO: Odstraněna mezera
+        gotoxy(x+w+1, y+i); printf("%c", 186); // OPRAVENO: Odstraněna mezera
     }
 
     gotoxy(x, y+h); printf("%c", 200);
@@ -110,10 +164,14 @@ void drawFrame(int x, int y, int w, int h, int color, char* title) {
 void tiskniNadpis() {
     gotoxy(0, 0);
     setCol(COL_CYAN);
-    printf("\n    %c%c %c%c%c %c%c%c %c%c %c%c %c%c%c %c%c%c %c%c%c%c%c %c%c%c%c%c \n", 219,219, 219,219,219, 219,219,219, 219,219, 219,219, 219,219,219, 219,219,219, 219,219,219,219,219, 219,219,219,219,219);
-    printf("    %c%c %c     %c   %c %c %c %c %c %c %c   %c     %c %c   \n", 219,219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219);
-    printf("    %c%c %c%c%c %c%c%c %c%c %c%c %c%c%c %c%c%c %c%c    %c%c%c  \n", 219,219, 219,219,219, 219,219,219, 219,219, 219,219, 219,219,219, 219,219,219, 219,219, 219,219,219);
-    printf("    -----------------------------------------------\n");
+    printf("\n");
+    printf("    ____ ____  _   _   __  __    _    ____ _____ _____ ____  \n");
+    printf("   / ___/ ___|| | | | |  \\/  |  / \\  / ___|_   _| ____|  _ \\ \n");
+    printf("   \\___ \\___ \\| |_| | | |\\/| | / _ \\ \\___ \\ | | |  _| | |_) |\n");
+    printf("    ___) |__) |  _  | | |  | |/ ___ \\ ___) || | | |___|  _ < \n");
+    printf("   |____/____/|_| |_| |_|  |_/_/   \\_\\____/ |_| |_____|_| \\_\\\n");
+    setCol(COL_GRAY);
+    printf("    -----------------------------------------------------------\n");
 }
 
 void statusLine() {
@@ -121,43 +179,48 @@ void statusLine() {
     setCol(COL_BG_BAR);
     for(int i=0; i<80; i++) printf(" ");
     gotoxy(2, 24);
-    printf(" ENTER: Connect | DEL: Delete | ESC: Exit | Current Nodes: %d", pocetServeru);
+    printf(" ENTER: Connect | DEL: Delete | ESC: Exit | Nodes: %d", pocetServeru);
     setCol(COL_DEFAULT);
 }
 
 void pridatPripojeni() {
-    system("cls"); // Zde cls nevadí, jsme mimo hlavní smyčku
+    system("cls");
+    fixEncoding();
     tiskniNadpis();
-    drawFrame(5, 7, 50, 9, COL_GREEN, " NEW CONNECTION SETUP ");
+    drawFrame(5, 8, 60, 10, COL_GREEN, " NEW CONNECTION SETUP "); // Mírně zvětšeno pro stabilitu
     
     setCol(COL_WHITE);
-    gotoxy(8, 9); printf("Alias Name   : "); setCol(COL_CYAN);
-    rewind(stdin);
-    fgets(servery[pocetServeru].jmeno, MAX_STR, stdin);
-    servery[pocetServeru].jmeno[strcspn(servery[pocetServeru].jmeno, "\n")] = 0;
+    gotoxy(8, 10); printf("Alias Nazev   : ");
+    inputWithPlaceholder(servery[pocetServeru].jmeno, MAX_STR, "Napr. Hlavni Server", 24, 10);
     
     setCol(COL_WHITE);
-    gotoxy(8, 11); printf("SSH Protocol : "); setCol(COL_GRAY); printf("ssh "); setCol(COL_CYAN);
-    fgets(servery[pocetServeru].adresa, MAX_STR, stdin);
-    servery[pocetServeru].adresa[strcspn(servery[pocetServeru].adresa, "\n")] = 0;
+    gotoxy(8, 12); printf("SSH Adresa    : "); setCol(COL_GRAY); printf("ssh ");
+    inputWithPlaceholder(servery[pocetServeru].adresa, MAX_STR, "root@192.168.0.101", 28, 12);
 
-    gotoxy(8, 13); setCol(COL_YELLOW); printf("Attempting RSA Key Handshake...");
+    gotoxy(8, 14); setCol(COL_YELLOW); printf("Attempting RSA Key Handshake...");
     char cmd[512];
-    _snprintf(cmd, sizeof(cmd), "type %%USERPROFILE%%\\.ssh\\id_rsa.pub | ssh %s \"mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys\"", servery[pocetServeru].adresa);
+    // OPRAVENO: Přidáno přesměrování do nul, aby chyby nerozbíjely UI
+    _snprintf(cmd, sizeof(cmd), "type %%USERPROFILE%%\\.ssh\\id_rsa.pub | ssh %s \"mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys\" >nul 2>&1", servery[pocetServeru].adresa);
     
-    if (system(cmd) == 0) {
+    showCursor();
+    int result = system(cmd);
+    fixEncoding();
+
+    if (result == 0) {
         pocetServeru++;
         saveToFile();
-        gotoxy(8, 14); setCol(COL_GREEN); printf("[ SUCCESS ] Node %s is authorized!  ", servery[pocetServeru-1].jmeno);
+        gotoxy(8, 16); setCol(COL_GREEN); printf("[ SUCCESS ] Node authorized!");
     } else {
-        gotoxy(8, 14); setCol(COL_RED); printf("[ FAILED ] Handshake rejected.         ");
+        gotoxy(8, 16); setCol(COL_RED); printf("[ FAILED ] Handshake rejected (Check password/IP).");
     }
+    hideCursor();
     setCol(COL_DEFAULT);
     _getch();
-    system("cls"); // Vyčistit po návratu do menu
+    system("cls");
 }
 
 int main() {
+    fixEncoding();
     char folderPath[MAX_PATH_LEN];
     char *userProfile = getenv("USERPROFILE");
     
@@ -175,28 +238,28 @@ int main() {
     system("cls");
 
     while (1) {
-        // Místo cls skočíme na začátek - klíč k plynulosti
+        fixEncoding();
         gotoxy(0, 0);
         tiskniNadpis();
         
         int celkem = 2 + pocetServeru;
-        drawFrame(4, 7, 52, celkem + 2, COL_FRAME, " INFRASTRUCTURE NODES ");
+        drawFrame(4, 8, 65, celkem + 2, COL_FRAME, " INFRASTRUCTURE NODES "); // Zvětšeno na 65
 
         for (int i = 0; i < celkem; i++) {
-            gotoxy(7, 9 + i);
+            gotoxy(7, 10 + i);
             if (vybrany == i) {
-                setCol(COL_CYAN); printf("%c ", 175); // Šipka
+                setCol(COL_CYAN); printf("%c ", 175);
                 setCol(COL_WHITE); 
-                if (i == 0) printf("[ + ] REGISTER NEW SERVER      ");
-                else if (i == celkem - 1) printf("[ X ] TERMINATE APPLICATION    ");
-                else printf("SSH: %-25s", servery[i-1].jmeno);
+                if (i == 0) printf("[ + ] REGISTER NEW SERVER             ");
+                else if (i == celkem - 1) printf("[ X ] TERMINATE APPLICATION           ");
+                else printf("Node: %-35s", servery[i-1].jmeno);
             } else {
-                printf("  "); // Smazat šipku
-                if (i == 0) { setCol(COL_GREEN); printf("  Add new server            "); }
-                else if (i == celkem - 1) { setCol(COL_RED); printf("  Exit program              "); }
+                printf("  "); 
+                if (i == 0) { setCol(COL_GREEN); printf("  Add new server                   "); }
+                else if (i == celkem - 1) { setCol(COL_RED); printf("  Exit program                     "); }
                 else { 
                     setCol(COL_GRAY); printf("  Node: ");
-                    setCol(COL_DEFAULT); printf("%-25s", servery[i-1].jmeno);
+                    setCol(COL_DEFAULT); printf("%-35s", servery[i-1].jmeno);
                 }
             }
         }
@@ -211,16 +274,17 @@ int main() {
             else if (kl == 83) { // DELETE
                 if (vybrany > 0 && vybrany < celkem - 1) {
                     setCol(COL_RED);
-                    gotoxy(7, 9 + celkem + 1); printf("Destroy node %s? (y/n): ", servery[vybrany-1].jmeno);
+                    gotoxy(7, 10 + celkem + 1); printf("Destroy node %s? (y/n): ", servery[vybrany-1].jmeno);
+                    showCursor();
                     if (tolower(_getch()) == 'y') {
                         for (int i = vybrany - 1; i < pocetServeru - 1; i++) servery[i] = servery[i+1];
                         pocetServeru--;
                         saveToFile();
-                        system("cls"); // Po smazání vyčistíme zbytky rámečku
+                        system("cls");
                     } else {
-                        // Smazat potvrzovací text
-                        gotoxy(7, 9 + celkem + 1); printf("                          ");
+                        gotoxy(7, 10 + celkem + 1); printf("                                     ");
                     }
+                    hideCursor();
                 }
             }
         } else if (kl == 13) {
@@ -229,14 +293,16 @@ int main() {
             else {
                 system("cls");
                 setCol(COL_GREEN); 
-                printf("\n    ===============================================\n");
-                printf("    [ INITIATING ] Connecting to: %s\n", servery[vybrany-1].jmeno);
-                printf("    ===============================================\n\n");
+                printf("\n    [ INITIATING ] Connecting to: %s\n", servery[vybrany-1].jmeno);
+                printf("    -----------------------------------------------\n\n");
                 setCol(COL_DEFAULT);
                 
                 char ssh_cmd[256];
                 _snprintf(ssh_cmd, sizeof(ssh_cmd), "ssh %s", servery[vybrany-1].adresa);
+                
+                showCursor();
                 system(ssh_cmd);
+                fixEncoding();
                 exit(0);
             }
         } else if (kl == 27) break;
